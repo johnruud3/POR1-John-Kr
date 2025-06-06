a68f581247f9015bae230cdd6129fd3ff453be03
johnruud3 <johnruud@hotmail.no>
2025-06-06 18:32:51 +0200

    Added contact page and about me page.
    Also added a hamburger menu.

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,8 @@
-hellooooo
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.getElementById("burger");
+  const navLinks = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});
